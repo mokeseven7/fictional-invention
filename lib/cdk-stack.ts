90974ae2c04d11551e48476a0da1df2862fe8ba6
Mike McGrath <mike@dopefruit.com>
2021-01-30 12:30:16 +0000

    inti

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,4 +1,6 @@
 import * as cdk from '@aws-cdk/core';
+import * as path from 'path';
+import { ServerlessLaravel } from 'cdk-serverless-lamp';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
